809729be54682f63d4a351043f9464ff8d26c047
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-04-14 21:17:31 +0600

    add traditional for loop in comment

--- 8th week/vehicle/src/main/java/arefin/Main.java
+++ 8th week/vehicle/src/main/java/arefin/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Vehicle> vehicles = new ArrayList<>();
 
-        // Creating objects
         vehicles.add(new Car("Honda", "Civic", 30000, "Black", 5));
         vehicles.add(new Motorcycle("Yamaha", "R15", 5000, "Black", false));
         vehicles.add(new Car("Mercedes", " G 580", 50000, "Black", 5));
@@ -17,5 +16,10 @@ public class Main {
         for (Vehicle v : vehicles) {
             v.display();
         }
+        /*Using traditional "for" loop
+        for (int i = 0; i < vehicles.size(); i++) {
+       Vehicle v = vehicles.get(i);
+      v.display();*/
     }
 }
+
